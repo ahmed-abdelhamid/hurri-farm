f7e23012b4428d23189946fe93829325983a5bd4
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2019-06-10 10:10:46 +0300

    Change dashboard layout

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Grid } from 'semantic-ui-react';
-import Card from '../../layouts/Card/Card';
+import { Container } from 'semantic-ui-react';
 import ProfileSettings from './ProfileSettings';
 
 const Home = ({ auth }) => {
   return (
     <Container>
-      <Grid>
-        <Grid.Row columns={2}>
-          <Grid.Column width={4}>
-            <Card header=" عدد المستخدمين" />
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <ProfileSettings adminData={auth} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <ProfileSettings adminData={auth} />
     </Container>
   );
 };
